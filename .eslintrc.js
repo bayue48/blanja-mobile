246910d92f3ee224ad28c7ca7e2cad2c6716d76b
Moch. Bayu Erich Dewangga <70324722+bayue48@users.noreply.github.com>
2021-01-04 15:14:00 +0700

    create alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _redux: './src/redux',
+          _routes: './src/routes',
+          _screens: './src/screens',
+          _styles: './src/styles',
+          _utils: './src/utils',
+        },
+      },
+    },
+  },
 };
