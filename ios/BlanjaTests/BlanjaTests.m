8769781d0bf6b995da38b2d2cb18ea2fac549b36
Moch. Bayu Erich Dewangga <70324722+bayue48@users.noreply.github.com>
2021-02-24 12:46:10 +0700

    prototype

--- ios/BlanjaTests/BlanjaTests.m
+++ ios/BlanjaTests/BlanjaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface blanjainTests : XCTestCase
+@interface BlanjaTests : XCTestCase
 
 @end
 
-@implementation blanjainTests
+@implementation BlanjaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
